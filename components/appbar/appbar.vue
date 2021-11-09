0bdd330b159f002b0c3f1e1f49ea0ce6c88c33ba
Npsaksit <saksit936@gmail.com>
2021-11-09 16:57:19 +0700

    Inv commit
    Completed Project

---
+++ components/appbar/appbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="appbar">
+    <v-app-bar color="#4d0026" flat>
+      <img src="~/assets/logo.png" />
+      <div class="txtbar">LCIT INVOICE EDI MANAGEMENT SYSTEM</div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appbar',
+  // dialog: false,
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+}
+</script>
+
+<style>
+.txtbar {
+  font-weight: bold;
+  margin-left: 10px;
+  color: white;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+<style>
+body,
+html,
+.v-application {
+  font-family: 'Work Sans', sans-serif;
+  min-width: 300px;
+}
+.v-card__title {
+  font-weight: bold;
+  color: #4d0026;
+}
+</style>
